c0a9715f24255ca489d98cf777b849249607f211
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-10-03 10:15:14 +0700

    API CRUD role user

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import express, { Request, Response} from 'express';
 
-const app = express();
+import router from './router/Routes';
 
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const app = express();
+app.use(express.json());
+
 
 app.get('/', (req: Request, res: Response) => {
   return res.status(200).send({
     response: 'Express Typescript',
   });
 });
+
+app.use(router);
 
 app.listen(process.env.APP_PORT, () => {
   console.log(
