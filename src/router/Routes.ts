bb9aeb6e2946cd646cc7e930783d459a41c540cc
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-10-04 15:12:49 +0700

    input validation

--- src/router/Routes.ts
+++ src/router/Routes.ts
@@ -4,6 +4,8 @@ import RoleController from '../controllers/RoleController';
 
 import UserController from '../controllers/UserController';
 
+import UserValidation from '../middleware/validation/UserValidation';
+
 const router = express.Router();
 
 router.get("/role", RoleController.GetRole);
@@ -13,6 +15,6 @@ router.delete('/role/:id', RoleController.DeleteRole);
 router.get('/role/:id', RoleController.GetRoleById);
 
 // User Routing
-router.post("/user/signup", UserController.Register)
+router.post("/user/signup", UserValidation.RegisterValidation, UserController.Register)
 
 export default router;
